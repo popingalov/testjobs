65bc6e23160bf31519f012fde2470bbb17e25e6f
Oleksandr Balitskiy <popingalov@gmail.com>
2022-11-18 04:05:17 +0300

    clear

--- src/helperFunc/takeGeocod.ts
+++ src/helperFunc/takeGeocod.ts
@@ -1,16 +1,28 @@
-export default async function takeGeocod(obj: { lat: number; long: number }) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-try {
+export default async function takeGeocod(
+  obj: { lat: number; long: number } | string,
+  geocoder: google.maps.Geocoder = new google.maps.Geocoder(),
+)  {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+  try {
+    let helper;
+
+    if (typeof obj !== 'string') {
       const normalize = { lat: obj.lat, lng: obj.long };
-    const geocoder = new google.maps.Geocoder();
-    let helper = await geocoder.geocode({
-      location: normalize,
-    });
+      helper = await geocoder.geocode({
+        location: normalize,
+      });
+    } else {
+      helper = await geocoder.geocode({
+        address: obj,
+      });
+    }
+
     const adres = helper.results[3].address_components;
     const result = `${adres[1].long_name}, ${adres[3].long_name}`;
     return result;
-} catch (error) {
-  //десь в океан полетіли
-  return null
-}
+  } catch (error) {
+    //десь в океан полетіли
+    return 'Мангеттен, Нью-Йорк Каунти';
   }
+}
--- src/helperFunc/takeStartState.ts
+++ src/helperFunc/takeStartState.ts
@@ -3,26 +3,15 @@ import miniApi from '../miniApi';
 import devProps from '../help.json';
 
 export default async function startState(page: number = 1) {
-    const oldObj = (await miniApi(page)) || devProps;
-
-    let newObj = [];
-    let limitText = 'Мангеттен, Нью-Йорк Каунти';
-
-    const limit = 5;
-    let test = 0;
-      for (const el of oldObj) {
-      if (test < limit) {
-        const oldObj = el;
-        const stringa = await takeGeocod(el.location) || limitText
-        oldObj.geoCode = stringa;
-        newObj.push(oldObj);
-        
-        test += 1;
-      } else {
-        const oldObj = el;
-        oldObj.geoCode = limitText;
-        newObj.push(oldObj);
-      }
-    }
-return oldObj
+  const oldObj = (await miniApi(page)) || devProps;
+const geocoder =  new google.maps.Geocoder();
+  let newObj = [];
+  for (const el of oldObj) {
+    const oldObj = el;
+    const stringa = await takeGeocod(el.location, geocoder);
+    oldObj.geoCode = stringa;
+    newObj.push(oldObj);
   }
+
+  return newObj;
+}
